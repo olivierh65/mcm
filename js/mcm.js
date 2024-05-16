614cf4c9492aee8fb486c6aa047cb154ee024753
olivierh65 <ol65@sfr.fr>
2024-05-16 15:26:17 +0200

    Commit initial

---
+++ js/mcm.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * mcm behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.mcm = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
